86217ae8061dcc812758966025c288f729118d01
Darron Park <darron1217@gmail.com>
2022-10-16 03:39:11 +0900

    feat: 입력한 주소로 잔액조회 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,45 @@
-import { Box } from '@mui/material';
+import { Box, Button, TextField, Toolbar, Typography } from '@mui/material';
+import { useCallback, useEffect, useState } from 'react';
+import { HttpJsonRpcConnector, LotusClient } from 'filecoin.js';
 import NavBar from '../components/NavBar';
 
+const httpConnector = new HttpJsonRpcConnector({
+  url: `https://api.node.glif.io`,
+});
+
+const lotusClient = new LotusClient(httpConnector);
+
 const Home = () => {
+  const [accountInput, setAccountInput] = useState(``);
+  const [account, setAccount] = useState(``);
+  const [balance, setBalance] = useState(`0`);
+
+  const load = useCallback(async () => {
+    if (!account) {
+      return;
+    }
+
+    const b = await lotusClient.wallet.balance(account);
+
+    setBalance(b);
+  }, [account]);
+
+  useEffect(() => {
+    load();
+  }, [load]);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <NavBar />
-      Hello World
+      <Toolbar>
+        <TextField
+          value={accountInput}
+          onChange={(e) => setAccountInput(e.target.value)}
+        />
+        <Button onClick={() => setAccount(accountInput)}>Load</Button>
+      </Toolbar>
+      <Typography>Hello, {account}</Typography>
+      <Typography>Balance : {balance}</Typography>
     </Box>
   );
 };
