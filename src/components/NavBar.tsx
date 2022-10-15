6acd2f3dcc014ca23c51a2ea9f73efb5febe463e
Darron Park <darron1217@gmail.com>
2022-10-16 01:13:37 +0900

    feat: material-icons 추가

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,62 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import { AccountCircle } from '@mui/icons-material';
+import { ListItemIcon, ListItemText, Menu, MenuItem } from '@mui/material';
+import React from 'react';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 function NavBar() {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+    const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
-        <AppBar>
+        <AppBar position="static">
             <Toolbar>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     Filenori
                 </Typography>
-                <Button color="inherit">Switch Network</Button>
+                <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleMenu}
+                    color="inherit"
+                >
+                    <AccountCircle />
+                </IconButton>
+                <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                >
+                    <MenuItem onClick={handleClose}>
+                        <ListItemIcon>
+                            <SettingsIcon fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText>Setting</ListItemText>
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                </Menu>
             </Toolbar>
         </AppBar>
     );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
+import { Box } from "@mui/material";
 import NavBar from "../components/NavBar";
 
 function Home() {
     return (
-        <div>
+        <Box sx={{ flexGrow: 1 }}>
             <NavBar />
             Hello World
-        </div>
+        </Box>
     );
 }
 
