fac85bf7188c3503bb34c9a525303383e8a6a1f2
Darron Park <darron1217@gmail.com>
2022-10-16 02:01:15 +0900

    feat: eslint + prettier, IDE 설정 추가

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,59 +7,59 @@ import { ListItemIcon, ListItemText, Menu, MenuItem } from '@mui/material';
 import React from 'react';
 import SettingsIcon from '@mui/icons-material/Settings';
 
-function NavBar() {
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+const NavBar = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
-    const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    return (
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Filenori
-                </Typography>
-                <IconButton
-                    size="large"
-                    aria-label="account of current user"
-                    aria-controls="menu-appbar"
-                    aria-haspopup="true"
-                    onClick={handleMenu}
-                    color="inherit"
-                >
-                    <AccountCircle />
-                </IconButton>
-                <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorEl}
-                    anchorOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                    }}
-                    open={Boolean(anchorEl)}
-                    onClose={handleClose}
-                >
-                    <MenuItem onClick={handleClose}>
-                        <ListItemIcon>
-                            <SettingsIcon fontSize="small" />
-                        </ListItemIcon>
-                        <ListItemText>Setting</ListItemText>
-                    </MenuItem>
-                    <MenuItem onClick={handleClose}>My account</MenuItem>
-                </Menu>
-            </Toolbar>
-        </AppBar>
-    );
-}
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Filenori
+        </Typography>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          onClick={handleMenu}
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: `top`,
+            horizontal: `right`,
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: `top`,
+            horizontal: `right`,
+          }}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+              <SettingsIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText>Setting</ListItemText>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>My account</MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default NavBar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
-import { Box } from "@mui/material";
-import NavBar from "../components/NavBar";
+import { Box } from '@mui/material';
+import NavBar from '../components/NavBar';
 
-function Home() {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <NavBar />
-            Hello World
-        </Box>
-    );
-}
+const Home = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <NavBar />
+      Hello World
+    </Box>
+  );
+};
 
 export default Home;
